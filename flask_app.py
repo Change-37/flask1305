e3076a35bfc3698ceb6860e07843487f6663819f
Change-37 <202110305@bssm.hs.kr>
2021-12-27 10:03:53 +0900

    second commit

--- flask_app.py
+++ flask_app.py
@@ -21,6 +21,10 @@ def about():
 @app.route("/show1")
 def show1():
     return render_template('img_test1.html', image_file='img/1.jpg')
+
+@app.route("/show2")
+def show2():
+    return render_template('img_test2.html', image_file='img/2.jpg')
  
 @app.route("/kma")
 def kma():
@@ -70,32 +74,29 @@ def kma1():
 
   
 @app.route("/kma2")
-def kma1():
-  target = request.urlopen("https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=2626057000")
+def kma2():
+  target = request.urlopen("https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=159")
  
   soup = BeautifulSoup(target, "html.parser")
  
   output = ""
-  tm = ""
  
   for item in soup.select("item"):
     output += "<h2>{}</h2><hr/>".format(item.select_one("title").string)
-
-  for item in soup.select("head"):
-    tm += item.select_one("tm").string
    
-  for data in soup.select("data"):
-    output += "<h3>{}</h3>".format(data.select_one("city").string)
-    output += "날짜: {}</br>".format(data.select_one("day").string)
-    output += "시간: {}:00</br>".format(data.select_one("hour").string)
-    output += "날씨: {}</br>".format(data.select_one("wfKor").string)
-    output += "예상 기온: {}</br>".format(data.select_one("temp").string)
-    output += "최저/최고 기온: {}/{}".format(data.select_one("tmn").string, data.select_one("tmx").string)
+  for location in soup.select("location"):
+    output += "<h3>{}</h3>".format(location.select_one("city").string)
+    output += "날짜: {}</br>".format(location.select_one("tmEf").string)
+    output += "날씨: {}</br>".format(location.select_one("wf").string)
+    output += "최저/최고 기온: {}/{}".format(location.select_one("tmn").string, location.select_one("tmx").string)
     output += "<hr/>"
  
   output += "{}</br>".format(soup.select_one("title").string)
-  output += "지역: {}</br>".format(item.select_one("category").string)
+  output += "날짜: {}</br>".format(location.select_one("tmEf").string)  
+  output += "지역: {}</br>".format(soup.select_one("province").string)
  
   return output
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000', debug=True)
--- kma2.py
+++ kma2.py
@@ -1,21 +1,19 @@
 from urllib import request
 from bs4 import BeautifulSoup
 
-# 우리집 예보
-target = request.urlopen("https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=2626057000")
+# 서울 예보
+target = request.urlopen("https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=159")
 
 
 soup = BeautifulSoup(target, "html.parser")
-with open(file="myhome-weather.xml", mode="a") as urlPage:
+with open(file="bnk-weather.xml", mode="a") as urlPage:
     urlPage.write("{}".format(soup))
 
-tm = soup.select_one("tm")
-print("도시:", soup.select_one("category").string)
-for data in soup.select("data"):
-    print("날씨:", data.select_one("wfKor").string)
-    print("최저기온:", data.select_one("tmn").string)
-    print("최고기온:", data.select_one("tmx").string)
-    print("예상기온:", data.select_one("temp").string)
-    print("날짜:", data.select_one("day").string)
-    print("시간:", data.select_one("hour").string, ": 00")
+
+for location in soup.select("location"):
+    print("도시:", location.select_one("city").string)
+    print("날씨:", location.select_one("wf").string)
+    print("최저기온:", location.select_one("tmn").string)
+    print("최고기온:", location.select_one("tmx").string)
+    print("날짜:", location.select_one("tmEf").string)
     print()
